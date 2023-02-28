750769a2af5378c88bc29e21671d43c157bdcc5a
Diego3207 <brizuelamer369.damb@gmail.com>
2023-02-27 22:33:14 -0600

    creando nivel 1, poniendo preguntas sin terminar

--- nivel1.php
+++ nivel1.php
@@ -4,8 +4,30 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Nivel 1</title>
+	<link rel="stylesheet" href="nivel1.css">
 </head>
 <body>
-	
+	<div id="preguntas">
+		<h1>Nivel 1</h1>
+		<section>
+			<form action="nivel.php" method="post">
+				<p>
+					1. ¿Qué fracciones representan los puntos señalados en la recta?
+					
+				</p>
+				<img src="src/primerEjercicio.png" alt="Imagen Fracciones">
+				Respuestas:
+				<br>
+				A) 1/2, 3/4, 6/7<br>
+				2/4<br>
+				1/4<br>
+				2/4<br>
+				<p>
+					2. Se le llama así a la intersección de las mediatrices de los segmentos que conforman un triángulo.
+					
+				</p>
+			</form>
+		</section>
+	</div>
 </body>
 </html>
