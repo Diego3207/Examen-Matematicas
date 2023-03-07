eb67b63a18f1ee251e72f028a7474084afd70d70
Diego3207 <brizuelamer369.damb@gmail.com>
2023-03-06 22:27:34 -0600

    acomodo de las carpetas e intentando personalizar un radio button

--- nivel1.php
+++ nivel1.php
@@ -1,27 +1,32 @@
-<?php include("../cabecera.php"); ?>
+<?php include("cabecera.php"); ?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
 	<meta charset="UTF-8">
 	<title>Nivel 1</title>
-	<link rel="stylesheet" href="../CSS/Niveles/nivel1.css">
+	<link rel="stylesheet" href="CSS/nivel1.css">
 </head>
 <body>
 	<div id="preguntas">
 		<h1>Nivel 1</h1>
 		<section>
 			<form action="nivel1.php" method="post">
-				<p>
+				<p id="pregunta1">
 					1. ¿Qué fracciones representan los puntos señalados en la recta?
-					
 				</p>
 				<img src="src/primerEjercicio.png" alt="Imagen Fracciones">
 				Respuestas:
 				<br>
-				A) 1/2, 3/4, 6/7<br>
-				2/4<br>
-				1/4<br>
-				2/4<br>
+				<label>
+					<input type="radio" name="radio-button" value="css" checked />
+					A)
+				</label>
+				<br>
+				<input type="radio" name="respuesta1"> 
+				A) 1/2, 3/4, 6/7
+				<br>
+				<input type="radio" name="respuesta1"> B) 1/2, 3/4, 6/7
+				
 				<p>
 					2. Se le llama así a la intersección de las mediatrices de los segmentos que conforman un triángulo.
 					
